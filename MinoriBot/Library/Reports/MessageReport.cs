47faf930da86d58c23ffcb2979efc8bf908b7159
咲花 白 <1970366034@qq.com>
2023-08-06 22:57:39 +0800

    支持了array的上报数据

--- MinoriBot/Library/Reports/MessageReport.cs
+++ MinoriBot/Library/Reports/MessageReport.cs
@@ -13,10 +13,27 @@ namespace MinoriBot.Library.Reports
         public string sub_type { get; set; }
         public int message_id { get; set; }
         public long user_id { get; set; }
-        public string message { get; set; }
+        public List<MessageReportMsg> message { get; set; }
         public string raw_message { get; set; }
         public int font { get; set; }
         public MessageSender sender { get; set; }
 
+    }
+    public class MessageReportMsg
+    {
+        public string type { get; set; }
+        public List<MessageReportMsgData> messageReportMsgDatas { get; set; }
+    }
+    public class MessageReportMsgData
+    {
+        public string text { get; set; }//文本
+        public string id { get; set; }//表情id
+        public string file { get; set; }//语音文件,短视频文件
+        public string qq { get; set; }//QQ号
+        public string name { get; set; }//此栏无效，此人在群里
+        public string url { get; set; }//链接分享
+        public string title { get; set; }//链接标题
+        public string audio { get; set; }//音频文件
+        public string dtype { get; set; }//表示音乐自定义分享456
     }
 }
