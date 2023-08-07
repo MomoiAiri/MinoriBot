bc5e8e7d5907e29a03254a2eb45e556dc0c453cc
咲花 白 <1970366034@qq.com>
2023-08-07 22:20:49 +0800

    实现生成单张卡面Icon

--- MinoriBot/Enums/Sekai/SkCards.cs
+++ MinoriBot/Enums/Sekai/SkCards.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,33 +30,103 @@ namespace MinoriBot.Enums.Sekai
         public List<CardParameters> cardParameters = new List<CardParameters>();
         public List<SpecialTrainingCosts> specialTraining = new List<SpecialTrainingCosts>();
         public List<MasterLessonAchieveResources> masterLessonAchieveResources = new List<MasterLessonAchieveResources>();
+        public class CardParameters
+        {
+            public int id;
+            public int cardId;
+            public int cardLevel;
+            public string cardParameterType;
+            public int power;
+        }
+        public class SpecialTrainingCosts
+        {
+            public int cardId;
+            public int seq;
+            public Cost cost;
+        }
+        public class Cost
+        {
+            public int resourceId;
+            public string resourceType;
+            public int resourceLevel;
+            public int quantity;
+        }
+        public class MasterLessonAchieveResources
+        {
+            public int releaseConditionId;
+            public int cardId;
+            public int masterRank;
+            public List<int> resources = new List<int>();
+        }
+        /// <summary>
+        /// 获取卡面Icon
+        /// </summary>
+        /// <param name="isTrained"></param>
+        /// <returns></returns>
+
+        public async Task<SKBitmap> GetCardIcon(bool isTrained)
+        {
+            string trainingStatus = isTrained ? "after_training" : "normal";
+            //三星以下及生日卡只有特训前
+            if (GetStarsCount() < 3) trainingStatus = "normal";
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string fileDirectory = basePath + $"asset/thumbnail/chara_rip";
+            string filePath = fileDirectory + $"/{assetbundleName}_{trainingStatus}.png";
+            if (!Directory.Exists(fileDirectory))
+            {
+                Directory.CreateDirectory(fileDirectory);
+            }
+            if (File.Exists(filePath))
+            {
+                SKBitmap sKBitmap = SKBitmap.Decode(filePath);
+                return sKBitmap;
+            }
+            else
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        byte[] imageBytes = await client.GetByteArrayAsync($"https://storage.sekai.best/sekai-assets/thumbnail/chara_rip/{assetbundleName}_{trainingStatus}.png");
+
+                        using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await fileStream.WriteAsync(imageBytes, 0, imageBytes.Length);
+                            Console.WriteLine($"保存图片{assetbundleName}成功");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Download Image Failed" + e);
+                        return null;
+                    }
+                }
+                SKBitmap sKBitmap = SKBitmap.Decode(filePath);
+                return sKBitmap;
+            }
+        }
+        /// <summary>
+        /// 获取卡牌的星级，0为生日卡
+        /// </summary>
+        /// <returns></returns>
+        public int GetStarsCount()
+        {
+            switch (cardRarityType)
+            {
+                case "rarity_1":
+                    return 1;
+                case "rarity_2":
+                    return 2;
+                case "rarity_3":
+                    return 3;
+                case "rarity_4":
+                    return 4;
+                case "rarity_birthday":
+                    return 0;
+            }
+            return -1;
+        }
+
     }
-    public class CardParameters
-    {
-        public int id;
-        public int cardId;
-        public int cardLevel;
-        public string cardParameterType;
-        public int power;
-    }
-    public class SpecialTrainingCosts
-    {
-        public int cardId;
-        public int seq;
-        public Cost cost;
-    }
-    public class Cost
-    {
-        public int resourceId;
-        public string resourceType;
-        public int resourceLevel;
-        public int quantity;
-    }
-    public class MasterLessonAchieveResources
-    {
-        public int releaseConditionId;
-        public int cardId;
-        public int masterRank;
-        public List<int> resources = new List<int>();
-    }
+    
 }
---
+++ MinoriBot/Utils/ImageCreater.cs
@@ -0,0 +1,64 @@
+﻿using MinoriBot.Enums.Sekai;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinoriBot.Utils
+{
+    internal class ImageCreater
+    {
+        public async void DrawCardIcon(SkCard card, bool isTrained)
+        {
+            string trainingStatus = isTrained ? "after_training" : "normal";
+            string fileDirectory = "./asset/normal";
+            using (SKBitmap bitmap = new SKBitmap(156, 180))
+            {
+                using (SKCanvas canvas = new SKCanvas(bitmap))
+                {
+                    canvas.Clear(SKColors.White); ;
+                    int starCount = card.GetStarsCount();
+                    if (starCount < 3)
+                    {
+                        trainingStatus = "normal";
+                        isTrained = false;
+                    }
+                    //画头像边框和星星
+                    if (starCount > 0)
+                    {
+                        canvas.DrawBitmap(await card.GetCardIcon(isTrained), new SKRect(0, 0, 156, 156));
+                        canvas.DrawBitmap(SKBitmap.Decode($"{fileDirectory}/cardIconFrame_{starCount}.png"), new SKRect(0, 0, 156, 156));
+                        for (int i = 0; i < starCount; i++)
+                        {
+                            canvas.DrawBitmap(SKBitmap.Decode($"{fileDirectory}/{trainingStatus}_star.png"), new SKRect(10 + i * 25, 121, 35 + i * 25, 146));
+                        }
+                    }
+                    if (starCount == 0)
+                    {
+                        canvas.DrawBitmap(await card.GetCardIcon(false), new SKRect(0, 0, 156, 156));
+                        canvas.DrawBitmap(SKBitmap.Decode($"{fileDirectory}/cardIconFrame_bd.png"), new SKRect(0, 0, 156, 156));
+                        canvas.DrawBitmap(SKBitmap.Decode($"{fileDirectory}/birthday_star.png"), new SKRect(10, 116, 40, 146));
+                    }
+                    //画属性
+                    canvas.DrawBitmap(SKBitmap.Decode($"{fileDirectory}/{card.attr}.png"), new SKRect(10, 10, 40, 40));
+                    //添加文字id
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        paint.TextSize = 20;
+                        paint.Color = SKColors.DarkGray;
+                        paint.TextAlign = SKTextAlign.Left;
+                        string text = "ID:" + card.id.ToString();
+                        canvas.DrawText(text, 5, 175, paint);
+                    }
+                    using (var image = SKImage.FromBitmap(bitmap))
+                    using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                    using (var stream = File.OpenWrite("./asset/temp/output.png"))
+                    {
+                        data.SaveTo(stream);
+                    }
+                }
+            }
+        }
+}
