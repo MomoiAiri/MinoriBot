e3ff8ed33bbee3357ed60eed61ec184e7aa231ac
咲花 白 <1970366034@qq.com>
2023-08-08 22:50:34 +0800

    查卡功能编写中

---
+++ MinoriBot/Utils/Routers/SearchInfo.cs
@@ -0,0 +1,44 @@
+﻿using MinoriBot.Enums.Sekai;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinoriBot.Utils.Routers
+{
+    internal static class SearchInfo
+    {
+        public static async Task SearchCharacter(string messgae)
+        {
+            string[] keywords = messgae.Split(' ');
+            Dictionary<string, string> keys = FuzzySearch.FuzzySearchCharacter(keywords);
+            List<SkCard> cards = new List<SkCard>();
+            
+        }
+        private static List<SkCard> FindMatchingCards(List<SkCard> cards,Dictionary<string,string> searchConditions)
+        {
+            var query = cards.AsQueryable();
+
+            foreach (var condition in searchConditions)
+            {
+                switch (condition.Key)
+                {
+                    case "attribute":
+                        query = query.Where(card => card.attr == condition.Value);
+                        break;
+                    case "group":
+                        query = query.Where(card => card.Name == condition.Value);
+                        break;
+                    case "character":
+                        query = query.Where(card => card.id == int.Parse(condition.Value));
+                        break;
+                        // 添加其他条件
+                }
+            }
+
+            return query.ToList();
+        }
+        
+    }
+}
