b3ddfdec60f07fe5f64c513328f5ece00e4cab30
咲花 白 <1970366034@qq.com>
2023-09-18 12:37:58 +0800

    优化启动逻辑

--- MinoriBot/Init.cs
+++ MinoriBot/Init.cs
@@ -1,4 +1,5 @@
-﻿using MinoriBot.Utils.StaticFilesLoader;
+﻿using MinoriBot.Enums;
+using MinoriBot.Utils.StaticFilesLoader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,19 @@ namespace MinoriBot
     {
         public static async Task Start()
         {
+            CreatConfigYaml();
             await SekaiImageDownload.InitNormalImage();
             await SkDataBase.Start();
+        }
+        public static void CreatConfigYaml()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string fileName = path + "config.yaml";
+            if (!File.Exists(fileName))
+            {
+                string configtxt = "#连接方式 1.正向ws  2.反向ws\r\nsocketMode: 1\r\n#监听端口，使用反向ws时填写\r\nlistenPort: 12001\r\n#ws地址，使用正向ws时填写 格式例子: \"ws://kohane.com:11451\"\r\nwsAddr: \"";
+                File.WriteAllText(fileName, configtxt);
+            }
         }
     }
 }
