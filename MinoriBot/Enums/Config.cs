c7bcc6ee376261f6b73de48f63b658b0726a2609
咲花 白 <1970366034@qq.com>
2023-10-27 13:34:53 +0800

    feat: 增加配置选项

--- MinoriBot/Enums/Config.cs
+++ MinoriBot/Enums/Config.cs
@@ -9,6 +9,8 @@ namespace MinoriBot.Enums
 {
     public class Config
     {
+        private static Config instance;
+
         public int listenPort;
         public string wsAddr;
         public int socketMode;
@@ -16,5 +18,42 @@ namespace MinoriBot.Enums
         public bool proxy;
         public string githubproxy;
         public string sdcvproxy;
+
+        private Config()
+        {
+
+        }
+
+        public static Config Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Config();
+            }
+            return instance;
+        }
+    }
+
+    public class TempConfig
+    {
+        public int listenPort;
+        public string wsAddr;
+        public int socketMode;
+        public int httpListenPort;
+        public bool proxy;
+        public string githubproxy;
+        public string sdcvproxy;
+
+        public void Init()
+        {
+            Config.Instance().listenPort = listenPort;
+            Config.Instance().wsAddr = wsAddr;
+            Config.Instance().socketMode = socketMode;
+            Config.Instance().httpListenPort = httpListenPort;
+            Config.Instance().proxy = proxy;
+            Config.Instance().githubproxy = githubproxy;
+            Config.Instance().sdcvproxy = sdcvproxy;
+            Console.WriteLine("配置加载完成");
+        }
     }
 }
